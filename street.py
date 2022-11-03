5986be80b9611f05878a314c7c6353c87902e70c
Jake Kravas <jakekravas@Jakes-MacBook-Air.local>
2022-11-02 18:58:16 -0700

    added class for parking lots

--- street.py
+++ street.py
@@ -21,3 +21,16 @@ class Building:
         self._width = width
         self._height = height
         self._brick = brick
+
+class Lot:
+    """
+        Holds data of parking lots
+    """
+    def __init__(self, width, trash):
+
+        # replace trash underscores with spaces
+        trash = self.remove_trash_underscores(trash)
+
+        self._width = width
+        self._trash = trash
+        self._height = 0
