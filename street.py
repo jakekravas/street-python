134bb21187197a7ea8476a4859868c6de60b945f
Jake Kravas <jakek237@arizona.edu>
2022-11-12 13:56:12 -0700

    added at_height methods to Park, Lot, and Building classes that retrieves height of instance

--- street.py
+++ street.py
@@ -15,6 +15,42 @@ class Park:
         self._width = width
         self._foliage = foliage
         self._height = 4
+    
+    def at_height(self, height):
+        """
+            Returns string of design of park at specific height
+        """
+
+        # if height is 0 or 1, return trunk of tree
+        # with equal amount of whitespace on both sides
+        if height == 0 or height == 1:
+            whitespace_amount = int((self._width - 1) / 2)
+            side_whitespace = ' ' * whitespace_amount
+            return side_whitespace + '|' + side_whitespace
+
+        # if height is 2, return 5 characters of self._foliage
+        # with equal amount of whitespace on both sides
+        elif height == 2:
+            whitespace_amount = int((self._width - 5) / 2)
+            side_whitespace = ' ' * whitespace_amount
+            return side_whitespace + (self._foliage * 5) + side_whitespace
+
+        # if height is 3, return 3 characters of self._foliage
+        # with equal amount of whitespace on both sides
+        elif height == 3:
+            whitespace_amount = int((self._width - 3) / 2)
+            side_whitespace = ' ' * whitespace_amount
+            return side_whitespace + (self._foliage * 3) + side_whitespace
+
+        # if height is 4, return 1 character of self._foliage
+        # with equal amount of whitespace on both sides
+        elif height == 4:
+            whitespace_amount = int((self._width - 1) / 2)
+            side_whitespace = ' ' * whitespace_amount
+            return side_whitespace + self._foliage + side_whitespace
+
+        # if height > 4, return whitespace of length self._width
+        return ' ' * self._width
 
 
 class Building:
@@ -26,6 +62,16 @@ class Building:
         self._height = height
         self._brick = brick
 
+    def at_height(self, height):
+        """
+            Returns string of design of building at specific height
+        """
+        if height <= self._height:
+            return self._brick * self._width
+
+        return ' ' * self._width
+
+
 class Lot:
     """
         Holds data of parking lots
@@ -33,11 +79,55 @@ class Lot:
     def __init__(self, width, trash):
 
         # replace trash underscores with spaces
-        # trash = self.remove_trash_underscores(trash)
+        trash = self.remove_trash_underscores(trash)
 
         self._width = width
         self._trash = trash
         self._height = 0
+
+    def at_height(self, height):
+        """
+            Returns string of design of lot at specific height
+        """
+        if height == 0:
+            trash = self._trash
+            # string of self._trash added onto itself
+            # until it's equal to length of self._width
+            trash_display_str = self.get_trash_str(trash)[:self._width]
+            return trash_display_str
+
+        return ' ' * self._width
+    
+    def remove_trash_underscores(self, trash):
+        """
+            Returns self._trash with underscores replaced with spaces
+        """
+
+        if len(trash) == 0:
+            return trash
+
+        if trash[0] == '_':
+            return ' ' + self.remove_trash_underscores(trash[1:])
+
+        return trash[0] + self.remove_trash_underscores(trash[1:])
+
+ 
+    def get_trash_str(self, trash):
+        """
+            Adds self._trash to string over and over
+            until the length of that string is
+            equal to self._width, then returns
+            that string
+        """
+
+        # if length of trash hasn't
+        # met or exceeded width, concatenate self._trash
+        # to it and call function recursively
+        if len(trash) < self._width:
+            trash += self._trash
+            return self.get_trash_str(trash)
+        
+        return trash
 
 
 def get_street_elements(street):
